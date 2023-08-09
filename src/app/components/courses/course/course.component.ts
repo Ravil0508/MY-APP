ea69252ca52558fe5b3d148a75a068f79e69c983
ganie <rganiev@diasoft.ru>
2023-08-09 22:26:35 +0300

    modul 2 commit

--- src/app/components/courses/course/course.component.ts
+++ src/app/components/courses/course/course.component.ts
@@ -1,17 +1,23 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import { Course } from "../../model/course";
+import { Course } from "../../../model/course";
+
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
 })
 export class CourseComponent {
   @Input() public course: Course = {} as Course;
   @Output() public editCourse: EventEmitter<Course> = new EventEmitter<Course>();
   @Output() public deleteCourse: EventEmitter<Course> = new EventEmitter<Course>();
 
-  public editCurs(): void {
+
+  public editCours(): void {
     this.editCourse.emit(this.course);
+  }
+
+  public deleteCours(): void {
+    this.deleteCourse.emit(this.course);
   }
 }
